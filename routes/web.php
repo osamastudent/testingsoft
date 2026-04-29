a02c4bba7d21adcbb137fd2ad526bd158bd1b17a
osamastudent <osamastudent321@gmail.com>
2026-04-29 15:51:49 +0500

    Yahan naya sahi message likhen update krdiya bhi second message

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::prefix('admin')->as('admin.')->group(function(){
+// Route::resource('products',ProductController::class);
+// });
+
+Route::resource('tests',TestController::class);
